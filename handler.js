9bba422caaf6159e66688ac06882a0f74e3b7566
znergy <ryanjonesirl@gmail.com>
2017-09-04 21:27:15 -0700

    add a way of testing the functions locally, by creating a json file to hold the pathParamaters that would be passed in, then pulling them out in the lambda function. This allows you to run, sls invoke local --function function_name --path lib/data.json.

--- handler.js
+++ handler.js
@@ -1,15 +1,25 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Working! Your function executed successfully!',
-      input: event,
-    }),
-  };
 
-  callback(null, response);
+  if(event.pathParameters && event.pathParameters.name) {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Hello, ${event.pathParameters.name}`
+      })
+    })
+  } else {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Working! Your function executed successfully!',
+        input: event,
+      }),
+    };
+
+    callback(null, response);
+  }
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
