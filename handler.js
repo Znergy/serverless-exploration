6a6960b2156fdfc2a23266c6c770e89ce5d9dbf2
znergy <ryanjonesirl@gmail.com>
2017-09-04 21:56:38 -0700

    add two different ways of implementing multiple lambda functions in a single js file and also setup another API Gateway endpoint for the second function. Local testing passed, using.. sls invoke local --function function_name lib/data.json, where the data.json contains a name property which is being checked for inside the lambda function. Downside, most likely will pass data in through queryStringParamaters so I'm unclear on the best approach to testing without rewriting two different ways which would to me defeat the purpose.

--- handler.js
+++ handler.js
@@ -1,7 +1,48 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+// module.exports = {
+//
+//   hello: function(event, context, callback) {
+//     if(event.pathParameters && event.pathParameters.name) {
+//       callback(null, {
+//         statusCode: 200,
+//         body: JSON.stringify({
+//           message: `Hello, ${event.pathParameters.name}`
+//         })
+//       })
+//     } else {
+//       const response = {
+//         statusCode: 200,
+//         headers: {
+//           "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+//           "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+//         },
+//         body: JSON.stringify({
+//           message: 'Working! Your function executed successfully!',
+//           input: event,
+//         }),
+//       };
+//
+//       callback(null, response);
+//     }
+//   },
+//   hello1: function(event, context, callback) {
+//     if(event.pathParameters && event.pathParameters.name) {
+//       callback(null, {
+//         statusCode: 200,
+//         body: JSON.stringify({
+//           message: `Hello, ${event.pathParameters.name}`
+//         })
+//       })
+//     }
+//   }
+//
+//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+//   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+// };
 
+/** Another way of writing multiple functions **/
+function hello(event, context, callback) {
   if(event.pathParameters && event.pathParameters.name) {
     callback(null, {
       statusCode: 200,
@@ -12,6 +53,10 @@ module.exports.hello = (event, context, callback) => {
   } else {
     const response = {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+      },
       body: JSON.stringify({
         message: 'Working! Your function executed successfully!',
         input: event,
@@ -20,7 +65,17 @@ module.exports.hello = (event, context, callback) => {
 
     callback(null, response);
   }
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+function hello1(event, context, callback) {
+  if(event.pathParameters && event.pathParameters.name) {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Hello, ${event.pathParameters.name}`
+      })
+    })
+  }
+}
+
+module.exports = { hello, hello1 };
